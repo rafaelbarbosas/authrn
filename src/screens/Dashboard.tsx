7c3dcf22f2dc31fcb19cd731611f924736b6539f
Rafael Barbosa <rafael.1940.b@gmail.com>
2022-05-19 23:16:50 -0300

    New component base to the screens

---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { View } from "react-native";
+
+const Dashboard : React.FC = () => {
+    return (
+        <View></View>
+    );
+}
+
+export default Dashboard;
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { View } from "react-native";
+
+const SignIn : React.FC = () => {
+    return (
+        <View></View>
+    );
+}
+
+export default SignIn;
