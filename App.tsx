3a7862917e9a679158730d31a2d4ba1bc80a3a53
Rafael Barbosa <rafael.1940.b@gmail.com>
2022-05-19 23:09:59 -0300

    Added libraries and Navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import "react-native-gesture-handler";
+
 import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>Teste</Text>
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text>Teste</Text>
+      </View>
+    </NavigationContainer>
   );
 };
 
